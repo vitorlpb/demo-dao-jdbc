4abd3bf1e0375b675833248bfcef326ecf750989
vitor <vitorborgespaes@gmail.com>
2022-01-21 14:34:58 -0300

    FindById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,12 +10,10 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-
-		Departments obj = new Departments(1,"Books");
-		
-		Seller seller = new Seller(21,"Bob","bob@gmail.com",new Date(),3000.0,obj);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
 	
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,11 +1,25 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.SellerDao;
+import model.entities.Departments;
 import model.entities.Seller;
 
 public class SellerDaoJDBC implements SellerDao {
+	
+	private Connection conn;
+	
+	public SellerDaoJDBC(Connection conn) {
+		this.conn = conn;
+		
+	}
 
 	@Override
 	public void insert(Seller obj) {
@@ -24,7 +38,37 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public Seller findById(Integer id) {
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try {
+			st = conn.prepareStatement("select seller.*, department.name as DepName "
+									 + "from seller "
+									 + "inner join department on seller.DEPARTMENTID = department.id "
+									 + "where seller.id = ?");
+			st.setInt(1, id);
+			rs = st.executeQuery();
+			if (rs.next()) {
+				Departments dep = new Departments();
+				dep.setId(rs.getInt("DEPARTMENTID"));
+				dep.setName(rs.getString("DepName"));
+				Seller obj = new Seller();
+				obj.setId(rs.getInt("ID"));
+				obj.setName(rs.getString("NAME"));
+				obj.setEmail(rs.getString("EMAIL"));
+				obj.setBaseSalary(rs.getDouble("BASESALARY"));
+				obj.setBirthdate(rs.getDate("BIRTHDATE"));
+				obj.setDepartments(dep);
+				return obj;
+			}
+			return null;
+		} catch (SQLException e) {
+			throw new  DbException(e.getMessage());
+		}finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+		
 	}
 
 	@Override
