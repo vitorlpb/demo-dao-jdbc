b1a8657d67831f42b7196ebd1d52d3cc1e22af46
vitor <vitorborgespaes@gmail.com>
2022-01-21 10:32:48 -0300

    Departments entity class

---
+++ src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import model.entities.Departments;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Departments obj = new Departments(1,"Books");
+		
+		System.out.println(obj);
+	
+	}
+
+}
